4180755483a4320e4eb6761c20936c2aff0593c0
MaGaroo <amoomajid99@gmail.com>
2020-03-08 19:40:59 +0330

    Refactor source code indentation

--- src/ternote.sh
+++ src/ternote.sh
@@ -6,45 +6,45 @@ mkdir -p $DATA_DIR
 PROG=`echo $0 | rev | cut -d'/' -f1 | rev`
 
 function list_notes
-  {
-    for note in `ls $DATA_DIR`
-      do
-        echo $note: `head -n 1 $DATA_DIR/$note`
-      done
-  }
+{
+  for note in `ls $DATA_DIR`
+    do
+      echo $note: `head -n 1 $DATA_DIR/$note`
+    done
+}
 
 function print_note
-  {
-    cat $DATA_DIR/$1
-  }
+{
+  cat $DATA_DIR/$1
+}
 
 function add_note
-  {
-    if [[ $# -ne 1 ]]
-      then
-        printf "note name? "
-        read note_name
-      else
-        note_name=$1
-    fi
-    vim $DATA_DIR/$note_name
-  }
+{
+  if [[ $# -ne 1 ]]
+    then
+      printf "note name? "
+      read note_name
+    else
+      note_name=$1
+  fi
+  vim $DATA_DIR/$note_name
+}
 
 function delete_note
-  {
-    rm $DATA_DIR/$1
-  }
+{
+  rm $DATA_DIR/$1
+}
 
 function print_help
-  {
-    cat <<EOF
+{
+  cat <<EOF
 Usage: $PROG l: list notes
        $PROG n [<note_title>]: add note
        $PROG p <note_title>: print note
        $PROG d <note_title>: delete note
        $PROG <any_thing_else>: show this help
 EOF
-  }
+}
 
 case $1 in
   l)
